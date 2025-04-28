b90cf58ba00dfd1e7481b3eba45a48c4308c30b3
Executor <admin@dream.day>
2025-04-28 17:20:01 +0800

    更新本地网易云喵块至1.2.7 (#9)
    更换获取用户信息接口以解决登录失败的问题
    解决检查登录状态失败的问题

--- ExtendNetease_DGJModule/Apis/NeteaseMusicApis.Login.cs
+++ ExtendNetease_DGJModule/Apis/NeteaseMusicApis.Login.cs
@@ -1,14 +1,16 @@
-﻿using ExtendNetease_DGJModule.Crypto;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ExtendNetease_DGJModule.Clients;
+using ExtendNetease_DGJModule.Crypto;
 using ExtendNetease_DGJModule.Exceptions;
 using ExtendNetease_DGJModule.Extensions;
 using ExtendNetease_DGJModule.Models;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ExtendNetease_DGJModule.Apis
 {
@@ -85,14 +87,31 @@ namespace ExtendNetease_DGJModule.Apis
         /// 异步创建用于二维码登录的key
         /// </summary>
         /// <exception cref="UnknownResponseException"></exception>
-        public static async Task<string> CreateUnikeyAsync(HttpClient client, CancellationToken token = default)
+        public static async Task<string> CreateUnikeyAsync(HttpClientv2 client, CancellationToken token = default)
         {
-            IDictionary<string, object> data = new Dictionary<string, object>(1)
+            var guid = Guid.NewGuid().ToString("n");
+            var ntesnnid = new Cookie("_ntes_nnid", $"{guid}%2C{Utils.DateTime2UnixTimeMillseconds(DateTime.Now)}", "/", ".163.com");
+            client.Cookies.Add(ntesnnid);
+            var ntesnuid = new Cookie("_ntes_nuid", guid, "/", ".163.com");
+            client.Cookies.Add(ntesnuid);
+            var iuqxldmzr = new Cookie("_iuqxldmzr_", "32", "/", ".163.com");
+            client.Cookies.Add(iuqxldmzr);
+            var jsessionId = new Cookie("JSESSIONID-WYYY", GetJSessionId(), "/", ".163.com");
+            client.Cookies.Add(jsessionId);
+            IDictionary<string, object> data = new Dictionary<string, object>(3)
             {
+                ["noCheckToken"] = true,
+                ["secureCaptcha"] = null,
                 ["type"] = 1
             };
             CryptoHelper.WebApiEncryptedData encrypted = CryptoHelper.WebApiEncrypt(data);
-            JObject root = (JObject)await client.PostAsync("https://music.163.com/weapi/login/qrcode/unikey", encrypted.GetContent(), token).GetJsonAsync(token).ConfigureAwait(false);
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, "https://music.163.com/weapi/login/qrcode/unikey");
+            req.Content = encrypted.GetContent();
+            req.Headers.Add("x-channelsource", "undefined");
+            req.Headers.Add("nm-gcore-status", "1");
+            req.Headers.Referrer = new Uri("https://music.163.com/");
+            req.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0");
+            JObject root = (JObject)await client.SendAsync(req, token).GetJsonAsync(token).ConfigureAwait(false);
             if (root["code"].ToObject<int>() == 200)
             {
                 return root["unikey"].ToObject<string>();
@@ -106,19 +125,41 @@ namespace ExtendNetease_DGJModule.Apis
         /// <exception cref="UnknownResponseException"></exception>
         public static async Task<UnikeyStatus> GetUnikeyStatusAsync(HttpClient client, string unikey, CancellationToken token = default)
         {
-            IDictionary<string, object> data = new Dictionary<string, object>(2)
+            IDictionary<string, object> data = new Dictionary<string, object>(4)
             {
                 ["key"] = unikey,
+                ["noCheckToken"] = true,
+                ["secureCaptcha"] = null,
                 ["type"] = 1
             };
             CryptoHelper.WebApiEncryptedData encrypted = CryptoHelper.WebApiEncrypt(data);
-            JObject root = (JObject)await client.PostAsync("https://music.163.com/weapi/login/qrcode/client/login", encrypted.GetContent(), token).GetJsonAsync(token).ConfigureAwait(false);
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, "https://music.163.com/weapi/login/qrcode/client/login");
+            req.Content = encrypted.GetContent();
+            req.Headers.Add("x-channelsource", "undefined");
+            req.Headers.Add("x-loginmethod", "QrCode");
+            req.Headers.Add("nm-gcore-status", "1");
+            req.Headers.Referrer = new Uri("https://music.163.com/");
+            req.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0");
+            JObject root = (JObject)await client.SendAsync(req, token).GetJsonAsync(token).ConfigureAwait(false);
             UnikeyStatus result = root["code"].ToObject<UnikeyStatus>();
             if (!Enum.IsDefined(typeof(UnikeyStatus), result))
             {
                 throw new UnknownResponseException(root);
             }
             return result;
+        }
+
+        private static string GetJSessionId()
+        {
+            const string src = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ\\/+";
+            StringBuilder sb = new StringBuilder(176);
+            Random r = new Random();
+            for (int i = 0; i < 176; i++)
+            {
+                sb.Append(src[r.Next(src.Length)]);
+            }
+            return sb.ToString();
+
         }
     }
 }
